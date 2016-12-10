2c9867fee3fac72aa8c1ad2e4254e2425dbe6c3f
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2016-12-10 17:50:36 +0100

    don't back up symlinks

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 RUNTIME=$(date "+%Y-%m-%dT%H:%M:%S")
 
 for src in bash_profile bashrc gitconfig gitignore_global irbrc tmux.conf vim; do
-	[ -f ~/.${src} -o -d ~/.${src} ] && mv ~/.${src} ${BASEDIR}/backups/${src}.${RUNTIME}
+	# remove existing symlinks:
+	[ -L ~/.${src} ] && rm ~/.${src}
+	# back up existing real files:
+	[ -e ~/.${src} ] && mv ~/.${src} ${BASEDIR}/backups/${src}.${RUNTIME}
 	ln -s ${BASEDIR}/${src} ~/.${src}
 done
