dcd8320d97d08994010b8e947d04251abc6062c5
Jeremy Huiskamp <jeremy.huiskamp@optiopay.com>
2017-10-29 18:55:40 +0100

    remove arcanist bash completion

--- bashrc.d/arcanist.sh
+++
@@ -1,8 +0,0 @@
-# TODO: publish a $BREW_PREFIX in main bashrc if more tools need this
-# calling brew adds a lot of time to shell startup...
-if command -v brew &>/dev/null; then
-	arcanist_completion="$(brew --prefix arcanist)/libexec/resources/shell/bash-completion"
-	if [ -r "$arcanist_completion" ]; then
-		source "$arcanist_completion"
-	fi
-fi
