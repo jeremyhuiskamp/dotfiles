5f86dcd2b340b3adddd8264ffc126bc07716f804
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2019-10-09 11:40:45 +0200

    make sure ~/.config exists

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 RUNTIME=$(date "+%Y-%m-%dT%H:%M:%S")
 
+mkdir -p ~/.config
+
 for src in config/nvim bash_profile bashrc bashrc.d gitconfig gitignore_global irbrc tmux.conf vim psqlrc; do
 	# remove existing symlinks:
 	[ -L ~/.${src} ] && rm ~/.${src}
