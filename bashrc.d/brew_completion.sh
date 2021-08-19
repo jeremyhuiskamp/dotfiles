130deb2ef9909885d5e7c39ac25fa0a83cf224f8
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2021-08-19 13:03:47 +0200

    updated brew bash completion
    Will probably never need this, but it was sitting around so...

--- bashrc.d/brew_completion.sh
+++ bashrc.d/brew_completion.sh
@@ -1,1 +1,12 @@
-[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+BREW_PREFIX=/usr/local
+if type brew &>/dev/null; then
+  if [[ -r "$BREW_PREFIX"/etc/bash_completion ]]; then
+    . "$BREW_PREFIX"/etc/bash_completion
+  elif [[ -r "$BREW_PREFIX"/etc/profile.d/bash_completion.sh ]]; then
+    . "$BREW_PREFIX"/etc/profile.d/bash_completion.sh
+  else
+    for COMPLETION in "$BREW_PREFIX"/etc/bash_completion.d/*; do
+      [[ -r "$COMPLETION" ]] && . "$COMPLETION"
+    done
+  fi
+fi
